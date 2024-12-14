e67c2f751efa8515ef2eced9fd6c3e1c60c7986b
Jason Smith <jasondsmith2501@gmail.com>
2024-12-14 09:49:51 -0800

    Added insertion sort

--- insertion.js
+++ insertion.js
@@ -1,3 +1,17 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            let temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+
+    return arr;
+}
+
+insertionSort([3, 20, 5]);
 
 module.exports = insertionSort;
