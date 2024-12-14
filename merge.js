39d807228d36422d1f9a73aff12a697a1473c4fe
Jason Smith <jasondsmith2501@gmail.com>
2024-12-14 10:15:38 -0800

    Added merge sort

--- merge.js
+++ merge.js
@@ -1,5 +1,40 @@
-function merge() {}
+function merge(left, right) {
+    let result = [];
+    let i = 0;
+    let j = 0;
 
-function mergeSort() {}
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            result.push(left[i]);
+            i++;
+        } else {
+            result.push(right[j]);
+            j++;
+        }
+    }
 
-module.exports = { merge, mergeSort};
+    while (i < left.length) {
+        result.push(left[i]);
+        i++;
+    }
+
+    while (j < right.length) {
+        result.push(right[j]);
+        j++;
+    }
+
+    return result;
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    let midIndex = Math.floor(arr.length / 2);
+    let leftArr = mergeSort(arr.slice(0, midIndex));
+    let rightArr = mergeSort(arr.slice(midIndex, arr.length));
+    return merge(leftArr, rightArr);
+}
+
+module.exports = { merge, mergeSort };
